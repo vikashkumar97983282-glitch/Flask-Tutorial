4104e3f254aadc47369d90f97292e2f359e4d4fc
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-14 02:03:03 +0530

    database folder structure.

---
+++ Database_structure/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///site.db'
+
+    db.init_app(app)
+    return app
