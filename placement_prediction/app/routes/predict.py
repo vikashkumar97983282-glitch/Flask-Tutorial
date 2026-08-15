86d572f089a1ce8b8e178d9eeef26edfa6f8ccef
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-15 23:16:53 +0530

    complete project and ready to use.

--- placement_prediction/app/routes/predict.py
+++ placement_prediction/app/routes/predict.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, redirect, Response, url_for, session, flash
+from flask import Blueprint, render_template, redirect, Response, url_for, session, flash,request
+import joblib
 
 
 
@@ -6,7 +7,31 @@ from flask import Blueprint, render_template, redirect, Response, url_for, sessi
 predict_bp = Blueprint('predict', __name__)
 
 
+with open("app/models/random_forest_regressor.pkl", "rb") as file:
+    model = joblib.load(file)
 
-@predict_bp.route('/predict')
+
+
+@predict_bp.route('/predict', methods=["GET","POST"])
 def predict():
+    if request.method == "POST":
+
+        cgpa = request.form.get("cgpa")
+        resume = request.form.get("resume")
+
+        input = [[
+            cgpa,
+            resume
+        ]]
+
+        prediction = model.predict(input)[0]
+
+        if prediction > 0.5:
+            result = "Likely to be Placed"
+        else:
+            result = "Less Likely to be Placed"
+
+        return render_template('result.html', prediction=result, score=prediction)
+
     return render_template('predict.html')
+
